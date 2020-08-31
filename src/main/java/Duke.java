3f088e286ff53c179231c06fa42efb72fdd52060
untitle4 <e0376922@u.nus.edu>
2020-09-01 01:10:41 +0800

    level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,16 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String line = "__________________________________";
         System.out.println("Hello from\n" + logo);
+        System.out.println(line);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("\n" + line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("\n" + line);
+
+
     }
 }
+
