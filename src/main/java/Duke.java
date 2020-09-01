602a9d75eb1d2a2044590123167d642ee3bc200b
untitle4 <e0376922@u.nus.edu>
2020-09-01 13:59:21 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+    public static void printGreeting(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -13,19 +13,34 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println(line + "\n");
+    }
+
+    public static void printLine(){
+        String line = "__________________________________";
+        System.out.println(line);
+    }
+
+    public static void printExit(){
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        printGreeting();
 
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
 
         while(!command.equalsIgnoreCase("Bye")) {
-            System.out.println(line);
+            printLine();
             System.out.println(command);
-            System.out.println(line + "\n");
+            printLine();
+            System.out.println();
             command = sc.nextLine();
         }
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line + "\n");
+        printExit();
 
     }
 }
